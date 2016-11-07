4ff6a69d484e7646b001400007f6ac014cd9a8a6
shchegrikovich <shchegrikovich@gmail.com>
2016-11-07 13:26:10 +0300

    Added converter

--- src/BynExchangeTelegramBot/Program.cs
+++ src/BynExchangeTelegramBot/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot;
@@ -16,6 +17,7 @@ namespace BynExchangeTelegramBot
     public class Program
     {
         private static readonly TelegramBotClient Bot = new TelegramBotClient(Environment.GetEnvironmentVariable("BYN-EXCHANGE-BOT-API-KEY"));
+        private static Regex Query = new Regex(@"^(?<amount>(\d)*([.,]\d)*) (?<currency>(\w)*)$", RegexOptions.Compiled, TimeSpan.FromSeconds(3));
         private static CancellationTokenSource TokenSource = new CancellationTokenSource();
 
         public static void Main(string[] args)
@@ -72,18 +74,65 @@ namespace BynExchangeTelegramBot
 
                     await Bot.SendTextMessageAsync(message.Chat.Id, text, replyMarkup: new ReplyKeyboardHide());
                 }
+                return;
             }
-            else
+            else if (Query.IsMatch(message.Text))
             {
-                var usage = @"Usage:
-/rate - returns current exchange rate
-";
-                await Bot.SendTextMessageAsync(message.Chat.Id, usage, replyMarkup: new ReplyKeyboardHide());
+                var match = Query.Match(message.Text);
+                var amount = float.Parse(match.Groups["amount"].Value);
+                var curAbbreviation = match.Groups["currency"].Value;
+
+                using (HttpClient client = new HttpClient())
+                {
+                    var now = DateTime.UtcNow;
+                    var date = now.ToString("yyyy-MM-dd");
+                    var allRates = await GetRates(client, date);
+
+                    var usd = allRates.FirstOrDefault(r => r.Cur_ID == 145);
+                    var eur = allRates.FirstOrDefault(r => r.Cur_ID == 292);
+                    var rus = allRates.FirstOrDefault(r => r.Cur_ID == 298);
+                    var gbp = allRates.FirstOrDefault(r => r.Cur_ID == 143);
+                    var ukr = allRates.FirstOrDefault(r => r.Cur_ID == 290);
+
+                    if (0 == String.Compare(curAbbreviation, "BYN", true))
+                    {
+                        var text = "Rates for " + now.ToString("MMMM dd") + ":\n";
+                        foreach (var rate in new[] { usd, eur, gbp, rus, ukr })
+                        {
+                            text += $"{amount} {curAbbreviation} = {amount * rate.Cur_Scale / rate.Cur_OfficialRate} {rate.Cur_Abbreviation}\n";
+                        }
+
+                        await Bot.SendTextMessageAsync(message.Chat.Id, text, replyMarkup: new ReplyKeyboardHide());
+                    }
+                    else
+                    {
+                        var cur = allRates.FirstOrDefault(r => 0 == String.Compare(r.Cur_Abbreviation, curAbbreviation, true));
+                        if (null != cur)
+                        {
+                            var text = "Rates for " + now.ToString("MMMM dd") + ":\n";
+                            var amountInByn = amount * cur.Cur_OfficialRate / cur.Cur_Scale;
+                            text += $"{amount} {curAbbreviation.ToUpper()} = {amountInByn} BYN\n";
+                            foreach (var rate in new[] { usd, eur, gbp, rus, ukr }.Where(r => r != cur))
+                            {
+                                text += $"{amount} {curAbbreviation} = {amountInByn * rate.Cur_Scale / rate.Cur_OfficialRate} {rate.Cur_Abbreviation}\n";
+                            }
+
+                            await Bot.SendTextMessageAsync(message.Chat.Id, text, replyMarkup: new ReplyKeyboardHide());
+                        }
+                    }
+                }
+                return;
             }
+            var usage = @"Usage:
+/rate - returns current exchange rate
+7 byn - converts to other currencies
+";
+            await Bot.SendTextMessageAsync(message.Chat.Id, usage, replyMarkup: new ReplyKeyboardHide());
         }
 
         private static async Task<ExchangeRate[]> GetRates(HttpClient client, string date)
         {
+            // https://www.nbrb.by/APIHelp/ExRates
             var url = $"http://www.nbrb.by/API/ExRates/Rates?onDate={date}&Periodicity=0";
             var resp = await client.GetAsync(url);
             var rawObj = await resp.Content.ReadAsStringAsync();
